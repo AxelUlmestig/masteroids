3ef38d6e47c0cd09c8694754391c33815507f61f
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-12-08 19:07:17 +0100

    make collision handling more robust

--- src/ProgressGameState/Collisions.hs
+++ src/ProgressGameState/Collisions.hs
@@ -2,7 +2,7 @@ module ProgressGameState.Collisions (handleCollisions) where
 
 import           Control.Lens    (at, over, view)
 import qualified Data.Map.Strict as M
-import           Data.Maybe      (fromJust)
+import           Data.Maybe      (fromMaybe)
 
 import           GameState       (GameState, gameStatePositionsL,
                                   gameStateVelocitiesL)
@@ -25,12 +25,20 @@ detectCollisions ((i, p):ps) = let
                                in collided ++ detectCollisions ps
 detectCollisions _           = []
 
+-- TODO: don't use default masses
 handleCollision :: (Int, Int) -> GameState -> GameState
 handleCollision (id1, id2) gs = let
-                                  p1 = fromJust $ view (gameStatePositionsL . at id1) gs
-                                  p2 = fromJust $ view (gameStatePositionsL . at id2) gs
-                                  v1 = fromJust $ view (gameStateVelocitiesL . at id1) gs
-                                  v2 = fromJust $ view (gameStateVelocitiesL . at id2) gs
-                                  (v1', v2') = bounce (p1, v1, defaultMass) (p2, v2, defaultMass) -- TODO: don't use default masses
-                                  update = M.insert id1 v1' . M.insert id2 v2'
+                                  maybeUpdate = do
+                                    p1 <- view (gameStatePositionsL . at id1) gs
+                                    v1 <- view (gameStateVelocitiesL . at id1) gs
+                                    let m1 = defaultMass
+
+                                    p2 <- view (gameStatePositionsL . at id2) gs
+                                    v2 <- view (gameStateVelocitiesL . at id2) gs
+                                    let m2 = defaultMass
+
+                                    let (v1', v2') = bounce (p1, v1, m1) (p2, v2, m2)
+                                    return $ M.insert id1 v1' . M.insert id2 v2'
+
+                                  update = fromMaybe id maybeUpdate
                                 in over gameStateVelocitiesL update gs
