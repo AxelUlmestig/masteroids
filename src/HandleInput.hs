c0bca876c86f8bc6b13bfd83e2e9bf1d572b848d
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-12-20 14:25:37 +0100

    wip

--- src/HandleInput.hs
+++ src/HandleInput.hs
@@ -2,8 +2,9 @@ module HandleInput (handleInput) where
 
 import           Control.Lens                       (set)
 import           Graphics.Gloss.Interface.Pure.Game (Event (EventKey, EventMotion, EventResize),
-                                                     Key (SpecialKey),
+                                                     Key (MouseButton, SpecialKey),
                                                      KeyState (Down, Up),
+                                                     MouseButton (LeftButton),
                                                      SpecialKey (KeySpace))
 
 import           GameState                          (GameState, gameHeight,
@@ -16,11 +17,12 @@ import           Physics                            (createV)
 -- thinks that it's in the bottom left. The EventMotion event is offset to
 -- accomodate this
 handleInput :: Event -> GameState -> GameState
-handleInput (EventMotion (mx, my)) gs                    = set gameStateMousePositionL (createV mx' my') gs
-                                                             where
-                                                               mx' = mx + fromIntegral (gameWidth gs) / 2
-                                                               my' = my + fromIntegral (gameHeight gs) / 2
-handleInput (EventKey (SpecialKey KeySpace) Down _ _) gs = set gameStateAcceleratingL True gs
-handleInput (EventKey (SpecialKey KeySpace) Up _ _) gs   = set gameStateAcceleratingL False gs
-handleInput (EventResize (width, height)) gs             = gs { gameWidth = width, gameHeight = height }
-handleInput _ gs                                         = gs
+handleInput (EventMotion (mx, my)) gs                       = set gameStateMousePositionL (createV mx' my') gs
+                                                                where
+                                                                  mx' = mx + fromIntegral (gameWidth gs) / 2
+                                                                  my' = my + fromIntegral (gameHeight gs) / 2
+handleInput (EventKey (SpecialKey KeySpace) Down _ _) gs    = set gameStateAcceleratingL True gs
+handleInput (EventKey (SpecialKey KeySpace) Up _ _) gs      = set gameStateAcceleratingL False gs
+handleInput (EventKey (MouseButton LeftButton) Down _ _) gs = gs -- TODO: fire laser
+handleInput (EventResize (width, height)) gs                = gs { gameWidth = width, gameHeight = height }
+handleInput _ gs                                            = gs
