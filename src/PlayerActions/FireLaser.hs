84b69e4b602fbd6af3280f141c2995c353ab22ab
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-12-27 11:06:02 +0100

    add laser recoil

--- src/PlayerActions/FireLaser.hs
+++ src/PlayerActions/FireLaser.hs
@@ -1,6 +1,6 @@
 module PlayerActions.FireLaser (fireLaser) where
 
-import           Control.Lens    (at, view)
+import           Control.Lens    (at, over, view)
 import qualified Data.Map.Strict as M
 import           Data.Maybe      (fromMaybe)
 
@@ -9,24 +9,28 @@ import           GameState       (EntityType (Player), GameState, addLaser,
                                   gameStateAnglesL, gameStateEntityTypesL,
                                   gameStatePositionsL, gameStateRadiiL,
                                   gameStateVelocitiesL)
-import           Physics         (addV, createV, movePosition, rotateV)
+import           Physics         (addV, createV, movePosition, rotateV, scaleV,
+                                  subtractV)
 
 
 fireLaser :: GameState -> GameState
 fireLaser gs = foldr ($) gs (fireFromPlayer <$> players gs)
 
 fireFromPlayer :: Int -> GameState -> GameState
-fireFromPlayer pid gs = fromMaybe gs gs'
-  where
-    gs' = do
-            (pos, vel, ang, rad) <- (,,,) <$> view (gameStatePositionsL . at pid) gs
-                                          <*> view (gameStateVelocitiesL . at pid) gs
-                                          <*> view (gameStateAnglesL . at pid) gs
-                                          <*> view (gameStateRadiiL . at pid) gs
+fireFromPlayer pid gs =
+  fromMaybe gs $ do
+    (pos, vel, ang, rad) <- (,,,) <$> view (gameStatePositionsL . at pid) gs
+                                  <*> view (gameStateVelocitiesL . at pid) gs
+                                  <*> view (gameStateAnglesL . at pid) gs
+                                  <*> view (gameStateRadiiL . at pid) gs
 
-            let laserPos = movePosition (rotateV ang (createV (rad + Constants.laserRadius + 1) 0)) pos
-            let laserVel = addV (rotateV ang Constants.laserBaseSpeed) vel
-            return $ addLaser laserPos laserVel ang gs
+    let laserPos = movePosition (rotateV ang (createV (rad + Constants.laserRadius + 1) 0)) pos
+    let laserVel = addV (rotateV ang Constants.laserBaseSpeed) vel
+
+    let playerMomentumDiff = scaleV (Constants.laserMass / Constants.playerMass) laserVel
+    let recoilPlayer = over gameStateVelocitiesL $ M.adjust (subtractV playerMomentumDiff) pid
+
+    return $ recoilPlayer $ addLaser laserPos laserVel ang gs
 
 players :: GameState -> [Int]
 players = fmap fst . filter ((==Player) . snd) . M.toList . view gameStateEntityTypesL
