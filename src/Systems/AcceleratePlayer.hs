5cb5f0fa624021e0b0fc740f73af2f0adbb8a1db
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-12-28 16:01:34 +0100

    use constants file in player acceleration

--- src/Systems/AcceleratePlayer.hs
+++ src/Systems/AcceleratePlayer.hs
@@ -3,14 +3,11 @@ module Systems.AcceleratePlayer (acceleratePlayer) where
 import           Control.Lens    (imap, over, view)
 import qualified Data.Map.Strict as M
 
+import qualified Constants
 import           GameState       (EntityType (Player), GameState, accelerating,
                                   gameStateAnglesL, gameStateEntityTypesL,
                                   gameStateVelocitiesL)
-import           Physics         (Acceleration, applyAcceleration, createV,
-                                  rotateV)
-
-playerAcceleration :: Acceleration
-playerAcceleration = createV 0.3 0
+import           Physics         (applyAcceleration, rotateV)
 
 acceleratePlayer :: GameState -> GameState
 acceleratePlayer gs = if not (accelerating gs)
@@ -19,7 +16,7 @@ acceleratePlayer gs = if not (accelerating gs)
                         foldr ($) gs
                         $ fmap (over gameStateVelocitiesL)
                         $ imap (flip M.adjust)
-                        $ fmap (applyAcceleration . flip rotateV playerAcceleration)
+                        $ fmap (applyAcceleration . flip rotateV Constants.playerAcceleration)
                         $ M.intersectionWith const (view gameStateAnglesL gs)
                         $ M.filter (==Player)
                         $ view gameStateEntityTypesL gs
