07e07183fed956ba19b1e9ff7afdc5b8319d548b
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-12-28 13:36:50 +0100

    refactor acceleration logic

--- src/Systems/AcceleratePlayer.hs
+++ src/Systems/AcceleratePlayer.hs
@@ -1,31 +1,25 @@
 module Systems.AcceleratePlayer (acceleratePlayer) where
 
-import           Control.Lens    (at, ix, over, view)
+import           Control.Lens    (imap, over, view)
 import qualified Data.Map.Strict as M
-import           Data.Maybe      (fromJust)
 
 import           GameState       (EntityType (Player), GameState, accelerating,
                                   gameStateAnglesL, gameStateEntityTypesL,
                                   gameStateVelocitiesL)
-import           Physics         (Acceleration, Angle, applyAcceleration,
-                                  createV, rotateV)
+import           Physics         (Acceleration, applyAcceleration, createV,
+                                  rotateV)
 
 playerAcceleration :: Acceleration
 playerAcceleration = createV 0.3 0
 
 acceleratePlayer :: GameState -> GameState
-acceleratePlayer gs = if accelerating gs
-                      then let
-                        update :: Int -> GameState -> GameState
-                        update pid = let
-                                       -- playerAngle will crash if the player angle is missing
-                                       playerAngle = fromJust $ view (gameStateAnglesL . at pid) gs :: Angle
-                                       acceleration = rotateV playerAngle playerAcceleration :: Acceleration
-                                     in over (gameStateVelocitiesL . ix pid) (applyAcceleration acceleration)
-
-                        setVelocities = foldr (.) id $ update <$> players gs
-                      in setVelocities gs
-                      else gs
-
-players :: GameState -> [Int]
-players = fmap fst . filter ((==Player) . snd) . M.toList . view gameStateEntityTypesL
+acceleratePlayer gs = if not (accelerating gs)
+                      then gs
+                      else
+                        foldr ($) gs
+                        $ fmap (over gameStateVelocitiesL)
+                        $ imap (flip M.adjust)
+                        $ fmap (applyAcceleration . flip rotateV playerAcceleration)
+                        $ M.intersectionWith const (view gameStateAnglesL gs)
+                        $ M.filter (==Player)
+                        $ view gameStateEntityTypesL gs
