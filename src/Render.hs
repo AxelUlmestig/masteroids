41f5fd6b7faf68ecd91614437a199d88cd99b2fc
Axel Ulmestig <axel.ulmestig@gmail.com>
2021-01-01 15:24:25 +0100

    refactor rendering

--- src/Render.hs
+++ src/Render.hs
@@ -1,8 +1,8 @@
 module Render (render) where
 
-import           Control.Lens    (at, view)
+import           Control.Lens    (at, imap, view)
 import qualified Data.Map.Strict as M
-import           Data.Maybe      (mapMaybe)
+import           Data.Maybe      (fromMaybe)
 import           Graphics.Gloss  (Picture (Blank, Pictures), rotate, scale,
                                   translate)
 
@@ -14,42 +14,15 @@ import           GameState       (EntityType (Asteroid, Laser, Player),
                                   gameStateAnglesL, gameStateEntityTypesL,
                                   gameStatePositionsL, gameStateRadiiL,
                                   gameWidth)
-import           Physics         (Angle (Angle), Position, Radius, createV,
-                                  movePosition, rotateV, toPair)
-
-data RenderData = PlayerRender Position Angle Bool
-                | AsteroidRender Position Angle Radius
-                | LaserRender Position Angle
+import           Physics         (Angle (Angle), toPair)
 
 render :: Assets -> GameState -> Picture
-render assets gs = renderLoopedSpace gs $ offsetGameWindow gs $ Pictures $ renderEntity assets <$> allRenderData gs
-
-allRenderData :: GameState -> [RenderData]
-allRenderData gs = let
-                     mPos eid           = view (gameStatePositionsL . at eid) gs
-                     mAng eid           = view (gameStateAnglesL . at eid) gs
-                     mRad eid           = view (gameStateRadiiL . at eid) gs
-
-                     f (eid, Player)    = PlayerRender <$> mPos eid <*> mAng eid <*> return (accelerating gs)
-                     f (eid, Asteroid)  = AsteroidRender <$> mPos eid <*> mAng eid <*> mRad eid
-                     f (eid, Laser)     = LaserRender <$> mPos eid <*> mAng eid
-                   in mapMaybe f . M.toList . view gameStateEntityTypesL $ gs
-
-renderEntity :: Assets -> RenderData -> Picture
-renderEntity assets (PlayerRender pos (Angle ang) acc) = Pictures [player, fire]
-  where
-    (x, y) = toPair pos
-    player = translate x y (rotate (-ang) (playerSprite assets))
-    fire = if acc then translate x' y' (rotate (-ang) (fireSprite assets)) else Blank
-      where
-        (x', y') = toPair $ movePosition (rotateV (Angle (180 + ang)) (createV 50 0)) pos
-renderEntity assets (AsteroidRender pos (Angle ang) rad) = translate x y $ rotate (-ang) $ scale s s $ asteroidSprite assets
-  where
-    (x, y)  = toPair pos
-    s       = rad / Constants.asteroidSpriteDefaultRadius
-renderEntity assets (LaserRender pos (Angle ang)) = translate x y (rotate (-ang) (laserSprite assets))
-  where
-    (x, y) = toPair pos
+render assets gs = renderLoopedSpace gs
+                  $ offsetGameWindow gs
+                  $ Pictures . fmap snd . M.toList
+                  $ setSpritePositions gs
+                  $ rotateSprites gs
+                  $ getSprites assets gs
 
 -- Gloss puts the origin in the middle of the screen by default. This puts it in the lower left corner
 offsetGameWindow :: GameState -> Picture -> Picture
@@ -61,3 +34,23 @@ renderLoopedSpace gs pic = let
                              xMax = fromIntegral $ gameWidth gs
                              yMax = fromIntegral $ gameHeight gs
                            in Pictures $ translate <$> [-xMax, 0, xMax] <*> [-yMax, 0, yMax] <*> return pic
+
+setSpritePositions :: GameState -> M.Map Int Picture -> M.Map Int Picture
+setSpritePositions gs = M.intersectionWith (uncurry translate . toPair) (view gameStatePositionsL gs)
+
+rotateSprites :: GameState -> M.Map Int Picture -> M.Map Int Picture
+rotateSprites gs = M.intersectionWith (rotate . negate . unwrap) (view gameStateAnglesL gs)
+  where
+    unwrap (Angle ang) = ang
+
+getSprites :: Assets -> GameState -> M.Map Int Picture
+getSprites assets gs = imap f $ view gameStateEntityTypesL gs
+  where
+    f _ Laser       = laserSprite assets
+    f eid Asteroid  = fromMaybe Blank $ do
+                        rad <- view (gameStateRadiiL . at eid) gs
+                        let s = rad / Constants.asteroidSpriteDefaultRadius
+                        return $ scale s s $ asteroidSprite assets
+    f _ Player      = if not (accelerating gs)
+                      then playerSprite assets
+                      else Pictures [playerSprite assets, translate (-50) 0 (fireSprite assets)]
